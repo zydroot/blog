65a1ec6e51175ee2342934ac913f9ad232e44fac
zydroot <zyd1722@163.com>
2017-03-21 08:39:18 +0800

    new
    add files

---
+++ app/Http/Controllers/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class Student extends Controller{
+
+    public function test11(){
+        return "controller test11";
+    }
+}
